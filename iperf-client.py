e00888aa9bed4eb40ffbd529922187ff48662e67
Ubuntu <ubuntu@ip-172-31-40-106.ec2.internal>
2014-06-10 13:59:30 +0000

    update iperf-client.py

--- iperf-client.py
+++ iperf-client.py
@@ -4,11 +4,19 @@ import os, sys
 import subprocess
 
 dst = sys.argv[1]
-cli_num = int(sys.argv[2])
+cli_range = int(sys.argv[2])
 
 port = 5201
 
-for i in range(0, cli_num):
-  p = subprocess.Popen(['sh', 'iperf-c.sh', dst, str(port), str(cli_num)],\
-      stdout=subprocess.PIPE)
-  port += 1
+for cli_num in range(1, cli_range+1):
+    iperfc = []
+    for i in range(0, cli_num):
+        p = subprocess.Popen(['sh', 'iperf-c.sh', dst, str(port), str(cli_num)],\
+        stdout=subprocess.PIPE)
+        iperfc.append(p)
+        port += 1
+    
+    for p in iperfc:
+        ret = p.wait()
+        print cli_num, "---", ret
+    
