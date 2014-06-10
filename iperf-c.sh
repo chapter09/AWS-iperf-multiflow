9fded0b1460503e377ada13dc897cbc2b0ba5360
Hao WANG <wh.sjtu@gmail.com>
2014-06-10 21:09:25 +0800

    add post-mark on the logfile

--- iperf-c.sh
+++ iperf-c.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-iperf -c $1 -t 180 -i 1 -p $2 > $1-$2-c.log
+iperf -c $1 -t 180 -i 1 -p $2 > $1-$2-c$3.log
--- iperf-client.py
+++ iperf-client.py
@@ -9,6 +9,6 @@ cli_num = int(sys.argv[2])
 port = 5201
 
 for i in range(0, cli_num):
-  p = subprocess.Popen(['sh', 'iperf-c.sh', dst, str(port)],\
+  p = subprocess.Popen(['sh', 'iperf-c.sh', dst, str(port), str(cli_num)],\
       stdout=subprocess.PIPE)
   port += 1
